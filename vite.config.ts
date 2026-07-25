f95cc11dee3fa2ea3830d61f2b0f486614772129
WanAdamm <wanadamworkmail@gmail.com>
2026-07-25 18:01:28 +0800

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "pages" ? "/CPI-Income-Dashboard/" : "/",
   plugins: [react(), tailwindcss()],
-})
+}));
